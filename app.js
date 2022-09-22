886ce78eb6a64fea734e9e18e9ccb33d7789666c
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2022-09-22 11:16:29 +0530

    update app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const nextBtn = document.querySelector("#check-button-next");
+const cashBox = document.querySelector(".cashing");
+const checkbtn = document.querySelector("#cash-button-cash");
+const returnChg = document.querySelector(".return");
 
 const availableNotes = [2000, 500, 100, 50, 20, 10 , 5, 1];
 
